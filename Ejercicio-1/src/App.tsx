408aa77f5d6c52c364bb14d1c94879c1ca519473
matiasmoscone1 <matiasmoscone1@gmail.com>
2025-11-27 17:10:52 -0300

    Mejorando ejercicio-1

--- Ejercicio-1/src/App.tsx
+++ Ejercicio-1/src/App.tsx
@@ -5,6 +5,7 @@ import './App.css'
 function App() {
   const [flagAddTask, setFlagAddTask] = useState(false);
   const [tasks, setTasks] = useState<Task[]>(data.array);
+  const [newTask, setNewTask] = useState<Task | null>(null);
 
   const popupRef = useRef<HTMLDivElement>(null);
 
@@ -16,7 +17,6 @@ function App() {
 
   const closePopup = (e: React.MouseEvent) => {
     const target = e.target as Node;
-
     if(popupRef.current && !popupRef.current.contains(target)){
       setFlagAddTask(false);
     };
@@ -30,6 +30,14 @@ function App() {
     });
   }
 
+  console.log(newTask);
+  const addTask = () => {
+    if(!newTask)return;
+    setTasks((prev) => [...prev, newTask]);
+    setNewTask(null);
+    setFlagAddTask(false);
+  }
+
 
 
   return (
@@ -37,12 +45,16 @@ function App() {
       <header>
         <h1>Lista de tareas</h1>
       </header>
-      <button id='btn-add-task' onClick={() => setFlagAddTask(true)}>Agregar Tarea</button>
+      <button id='btn-add-task' onClick={() => {setNewTask({id: tasks.length + 1, title: "", completed: false });setFlagAddTask(true)}}>Agregar Tarea</button>
       <section>
         {flagAddTask ? <div className='popup-container' onClick={(e) => closePopup(e)}>
           <div className='popup' ref={popupRef}>
-            <textarea cols={25} rows={25}/>
-            <input type='checkbox' />
+            <h3>Tarea nueva</h3>
+            <div><label>Id: </label><span>{tasks.length}</span></div>
+            <div><label>Titulo: </label><textarea cols={40} rows={2} value={newTask?.title || ""} 
+              onChange={(e) => setNewTask((prev) => prev ? {...prev, title: e.target.value} : null)}/></div>
+            <div><label>Estado: </label><input type='checkbox' onChange={() => setNewTask((prev) => prev ? {...prev, completed: !prev.completed} : null)} /></div>
+            <button onClick={() => addTask()}>Agregar</button>
           </div>
         </div> : <></>}
         <div className='task-container'>
