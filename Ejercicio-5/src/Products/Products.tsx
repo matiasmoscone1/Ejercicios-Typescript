56da4a54fd6f2d61b1f50bdd2de67705afe82ecf
matiasmoscone1 <matiasmoscone1@gmail.com>
2025-12-04 18:29:08 -0300

    Renderizando los productos

--- Ejercicio-5/src/Products/Products.tsx
+++ Ejercicio-5/src/Products/Products.tsx
@@ -1,12 +1,18 @@
 
+const images = ["notebook_1.jpg", "notebook_2.jpg", "phone_1.jpg", "phone_2.jpg", 
+"phone_3.jpg", "phone_4.jpg", "pc_desktop_1.jpg", "mouse.jpg", "keyboard.jpg", 
+"keyboard_2.jpg", "headset_1.jpg", "headset_2.jpg"];
 
 const Products = () => {
 
 
     return(<div className="products-container">
-
+        {images.map((imgName, i) => {
+            return(<div key={i} className="img-container">
+                <img src={`../public/images/${imgName}`} alt=""/>
+            </div>)
+        })}
     </div>)
-
 
 }
 
