323798de1a7fb0f59502291e2298004911df96f3
matiasmoscone1 <matiasmoscone1@gmail.com>
2025-12-06 20:54:46 -0300

    Debuggeando problema del dropdown y el carrito, se implemento dos ref

--- Ejercicio-5/src/Cart/Cart.tsx
+++ Ejercicio-5/src/Cart/Cart.tsx
@@ -1,19 +1,43 @@
-import { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useShopping } from "../Context/ShoppingCartProvider";
 
 const Cart = () => {
 
     const [state, setState] = useState(false);
 
+    const cartRef = useRef<HTMLDivElement>(null);
+    const secondRef = useRef<HTMLDivElement>(null);
+
     const globalState = useShopping();
+
+    
+
+    useEffect(() => {
+        
+        if(!state) return;
+
+        const closeDropdown = (e: MouseEvent) => {
+            const target = e.target as Node;
+            if(secondRef.current?.contains(target)){
+                return;
+            }
+            if(cartRef.current && !cartRef.current.contains(target)){
+                setState(false);
+            }
+        }
+
+        document.addEventListener("mousedown", closeDropdown)
+
+        return () => document.removeEventListener("mousedown", closeDropdown)
+    }, [state]);
 
     console.log(globalState);
 
     return(<div className="cart">
-        <i className="fa-solid fa-cart-shopping" onClick={() => setState(!state)}></i>
-        {state ? <div className="dropdown-cart-container">
+        <i className="fa-solid fa-cart-shopping" onClick={() => setState(!state)} ref={secondRef}></i>
+        {state ? <div className="dropdown-cart-container" ref={cartRef}>
             {globalState?.state.productsCart.map((prod) => {
-                return(<>
+                return(
                 <div key={prod.id} className="cart-product">
                     <span>
                         {prod.quantity}
@@ -23,7 +47,7 @@ const Cart = () => {
                     </span>
                     <button onClick={() => globalState?.dispatch({type: "REMOVE", payload: prod.id})}>X</button>
                 </div>
-                </>)
+                )
             })}
             <div className="total-price-cart">
                 <span>Precio total: ${globalState?.state.totalPrice}</span>
